ee02a3e0282c0f613a992edde099416c85c3a9cd
DavidIoana <ioana-laura.david@student.upt.ro>
2022-05-15 13:13:58 +0300

    SBA-65 Saving the appointment in a database

--- src/main/java/org/loose/fis/controllers/SalonListController.java
+++ src/main/java/org/loose/fis/controllers/SalonListController.java
@@ -3,14 +3,20 @@ package org.loose.fis.controllers;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import org.loose.fis.DataBaseUtil;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -26,6 +32,9 @@ public class SalonListController implements Initializable {
     @FXML
     private Button selectBtn;
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
 
     @Override
@@ -56,13 +65,33 @@ public class SalonListController implements Initializable {
         selectBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                DataBaseUtil.changeScene(event, "/Salon.fxml",accountListView.getSelectionModel().getSelectedItem(),null);
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("/Salon.fxml"));
+                try {
+                    root = loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                salonController salonController = loader.getController();
+                salonController.setSalonName(accountListView.getSelectionModel().getSelectedItem());
+                salonController.setClient(name);
+                stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.setTitle(accountListView.getSelectionModel().getSelectedItem());
+                stage.show();
             }
         });
 
         File listFile = new File("src/main/resources/docs/comb.png");
         Image listImage = new Image(listFile.toURI().toString());
         comb.setImage(listImage);
+    }
+
+    private String name;
+    public void setClientUsername(String clientName) {
+        name = clientName;
     }
 
 
