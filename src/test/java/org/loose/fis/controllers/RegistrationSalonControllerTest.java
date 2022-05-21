9bfe6a942e30f70ed943d856f7a210d966210b0a
DavidIoana <ioana-laura.david@student.upt.ro>
2022-05-21 22:07:37 +0300

    SBA-82 Testing the registration for a salon

---
+++ src/test/java/org/loose/fis/controllers/RegistrationSalonControllerTest.java
@@ -0,0 +1,74 @@
+package org.loose.fis.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+@ExtendWith(ApplicationExtension.class)
+class RegistrationSalonControllerTest {
+    public static final String USERNAME = "usernameSalonTest";
+    public static final String NAME = "nameSalonTest";
+    public static final String PHONE = "0744445123";
+    public static final String ADDRESS= "Timisoara";
+    public static final String PASSWORD="passwordSalonTest";
+
+    @BeforeEach
+    void setUp()
+    {
+        ConnectionDB.getDBConnection();
+    }
+
+    @Start
+    void start(Stage primaryStage)throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("registration.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    @Test
+    void testRegistrationSalon(FxRobot robot)
+    {
+        robot.clickOn("#salon_name_registration");
+        robot.write(NAME);
+        robot.clickOn("#salon_username_registration");
+        robot.write(USERNAME);
+        robot.clickOn("#salon_phone_registration");
+        robot.write(PHONE);
+        robot.clickOn("#salon_adress_registration");
+        robot.write(ADDRESS);
+        robot.clickOn("#salon_password_registration");
+        robot.write(PASSWORD);
+
+        robot.clickOn("#salon_register");
+    }
+
+    @Test
+    void testRegistrationSalonIfAllDataIsBlank(FxRobot robot)
+    {
+        robot.clickOn("#salon_name_registration");
+        robot.clickOn("#salon_username_registration");
+        robot.clickOn("#salon_phone_registration");
+        robot.clickOn("#salon_adress_registration");
+        robot.clickOn("#salon_password_registration");
+
+        robot.clickOn("#salon_register");
+    }
+
+    @Test
+    void testRegistrationSalonIfIsAlreadyAMember(FxRobot robot)
+    {
+        robot.clickOn("#SignInButton");
+    }
+
+
+
+
+}
