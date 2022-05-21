26bf249d259910b3e7559823d5b6702d5d630b23
DavidIoana <ioana-laura.david@student.upt.ro>
2022-05-21 23:36:40 +0300

    SBA-88 Testing to see the list of salons

---
+++ src/test/java/org/loose/fis/controllers/SalonListControllerTest.java
@@ -0,0 +1,39 @@
+package org.loose.fis.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(ApplicationExtension.class)
+class SalonListControllerTest {
+
+    @BeforeEach
+    void setUp()
+    {
+        ConnectionDB.getDBConnection();
+    }
+
+    @Start
+    void start(Stage primaryStage)throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("salonList.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    @Test
+    void testLoginClient(FxRobot robot)
+    {
+        robot.clickOn("#selectBtn");
+    }
+
+}
